59c204b435d880fed598cd0d93ca575633e98170
WaleedRazaShoaib <146237300+WaleedRazaShoaib@users.noreply.github.com>
2023-12-20 00:49:44 +0500

    date methods practice

--- app.js
+++ app.js
@@ -120,7 +120,51 @@
 // console.log(latestArr)
 
 
-let Array12 = [1,2,3,5,7,8,9,9];
-let each = Array12.forEach ((element,val)=>  {
-    console.log(element,val)
-});
+// Get the current date and time
+var today = new Date();
+
+// Get the day of the week (0-6, where 0 is Sunday and 6 is Saturday)
+var day = today.getDay();
+
+// Array of day names
+var daylist = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+// Display the current day
+console.log("Today is: " + daylist[day] + ".");
+
+// Get the current hour, minute, and second
+var hour = today.getHours();
+var minute = today.getMinutes();
+var second = today.getSeconds();
+
+// Determine if it's AM or PM
+var prepand = (hour >= 12) ? " PM " : " AM ";
+
+// Convert 24-hour format to 12-hour format
+hour = (hour >= 12) ? hour - 12 : hour;
+
+// Check for special cases when hour is 0
+if (hour === 0 && prepand === ' PM ') {
+    if (minute === 0 && second === 0) {
+        hour = 12;
+        prepand = ' Noon';
+    } else {
+        hour = 12;
+        prepand = ' PM';
+    }
+}
+
+// Check for special cases when hour is 0
+if (hour === 0 && prepand === ' AM ') {
+    if (minute === 0 && second === 0) {
+        hour = 12;
+        prepand = ' Midnight';
+    } else {
+        hour = 12;
+        prepand = ' AM';
+    }
+}
+
+// Display the current time
+    console.log("Current Time: " + hour + prepand + " : " + minute + " : " + second)
+
